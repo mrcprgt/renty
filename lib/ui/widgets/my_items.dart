4965a7114fc530139d6f2a8b087373e0f8f6a271
Marc Christian E. Pragata <mpragata@gmail.com>
2020-03-24 00:22:50 +0800

    deleted unused stuff

--- lib/ui/widgets/my_items.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class MyItemsPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(child: Text('My items'));
-  }
-}
