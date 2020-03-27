22e78c45162a25e5b7c9b7361ec67993eed43db1
Marc Christian E. Pragata <mpragata@gmail.com>
2020-03-27 16:31:04 +0800

    initial account view model for accounts view

---
+++ lib/viewmodels/accounts_view_model.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:renty_crud_version/constants/route_names.dart';
+import 'package:renty_crud_version/services/authentication_service.dart';
+import 'package:renty_crud_version/services/navigation_service.dart';
+import 'package:renty_crud_version/ui/views/login_view.dart';
+import 'package:renty_crud_version/viewmodels/base_model.dart';
+
+import '../locator.dart';
+
+class AccountsViewModel extends BaseModel {
+  final AuthenticationService _authenticationService =
+      locator<AuthenticationService>();
+  final NavigationService _navigationService = locator<NavigationService>();
+
+  Future logOut() async {
+    await _authenticationService
+        .logOut()
+        .whenComplete(() => {_navigationService.navigateTo(LoginViewRoute)});
+  }
+
+  List getUserDetails() {
+    var fullName = _authenticationService.currentUser.fullName;
+    var emailAddress = _authenticationService.currentUser.email;
+    var contactNumber = _authenticationService.currentUser.contactNumber;
+
+    var userDetailsMap = [fullName, emailAddress, contactNumber];
+    return userDetailsMap;
+  }
+}
