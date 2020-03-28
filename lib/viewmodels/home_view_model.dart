ea7785942c4f7140ec70d158089c1f0dcfdf9bff
Marc Christian E. Pragata <mpragata@gmail.com>
2020-03-28 20:32:58 +0800

    removed unused operations stuff

--- lib/viewmodels/home_view_model.dart
+++ lib/viewmodels/home_view_model.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/services.dart';
 import 'package:renty_crud_version/constants/route_names.dart';
 import 'package:renty_crud_version/models/operations.dart';
 import 'package:renty_crud_version/models/item.dart';
@@ -10,9 +9,6 @@ import '../locator.dart';
 import 'base_model.dart';
 
 class HomeViewModel extends BaseModel {
-  HomeViewModel() {
-    fetchOperations();
-  }
   // final AuthenticationService _authenticationService =
   //     locator<AuthenticationService>();
   final NavigationService _navigationService = locator<NavigationService>();
@@ -22,14 +18,8 @@ class HomeViewModel extends BaseModel {
   List<Item> _items;
   List<Item> get items => _items;
 
-  Operations _operations;
-  Operations get operations => _operations;
-
-  // List<String> categories = operations.categoriesMap.toList();
-
   void listenToItemListings() {
     setBusy(true);
-    //print('Item list: ' + items.toString());
     _firestoreService.listenToItemRealTime().listen((itemListingsData) {
       List<Item> updatedItemListing = itemListingsData;
       if (updatedItemListing != null && updatedItemListing.length > 0) {
@@ -42,6 +32,8 @@ class HomeViewModel extends BaseModel {
       setBusy(false);
     });
   }
+
+  void requestMoreData() => _firestoreService.requestMoreData();
 
   List<String> getAllItemNames() {
     List<String> itemNamesList = [];
@@ -56,38 +48,11 @@ class HomeViewModel extends BaseModel {
     List<dynamic> suggestionsList = [];
     pattern = pattern.toLowerCase();
     for (var i in allItemsList) {
-      if (i.contains(pattern)){
+      if (i.contains(pattern)) {
         suggestionsList.add(i);
       }
     }
     return suggestionsList;
-  }
-
-  Future fetchOperations() async {
-    //TODO: FIX CATEGORIES ASAP
-    try {
-      Operations operationsResults =
-          await _firestoreService.getOperationsFromDb();
-      await _firestoreService.getOperationsFromDb().then((val) {
-        //print('val categ: ' + val.categoriesMap);
-        return val;
-      });
-
-      //print('oper res ' + operationsResults.toString());
-      if (operationsResults.categoriesMap == null) {
-        //print('oper?null');
-      } else {
-        //print('oper has val');
-        //print(operationsResults.serviceFee.toString());
-        _operations = operationsResults;
-      }
-    } catch (e) {
-      if (e is PlatformException) {
-        return e.toString();
-      }
-    }
-
-    return;
   }
 
   void goToItemDetailPage(Item item) {
