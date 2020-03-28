cb8a62fadff94f910d708a599d2160cad3b78c9b
Marc Christian E. Pragata <mpragata@gmail.com>
2020-03-28 20:34:49 +0800

    converted to stateful widget to disallow page destruction

--- lib/ui/views/home_view.dart
+++ lib/ui/views/home_view.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:provider_architecture/viewmodel_provider.dart';
+import 'package:renty_crud_version/ui/widgets/creation_aware_list_item.dart';
 import 'package:renty_crud_version/ui/widgets/item_listing_tile.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:renty_crud_version/viewmodels/home_view_model.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({
     Key key,
   }) : super(key: key);
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
 
   @override
   Widget build(BuildContext context) {
     return ViewModelProvider<HomeViewModel>.withConsumer(
       viewModel: HomeViewModel(),
       onModelReady: (model) => model.listenToItemListings(),
-      builder: (context, model, child) => WillPopScope(
-        onWillPop: () => onWillPop(),
-        child: SafeArea(
+      builder: (context, model, child) {
+        super.build(context);
+        return SafeArea(
           child: Scaffold(
             body: model.items != null
                 ? Padding(
@@ -39,64 +49,10 @@ class HomeView extends StatelessWidget {
               child: Icon(Icons.add),
               onPressed: () => model.goToItemLendPage(),
             ),
-            // bottomNavigationBar: _buildBottomNavBar(context, model),
           ),
-        ),
-      ),
+        );
+      },
     );
-  }
-
-  Widget _buildCategoriesBar(BuildContext context, HomeViewModel model) {
-    return SliverToBoxAdapter(
-        child: Container(
-      height: 60.0,
-      //TODO: CHANGE THIS TO STREAM CATEGORIES
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 3,
-        // itemCount: model.operations.categoriesMap.length,
-        //model.categories.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              //crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                ClipOval(
-                  child: Material(
-                    color: Colors.pink, // button color
-                    child: InkWell(
-                      splashColor: Colors.red, // inkwell color
-                      // child: SizedBox(
-                      //     width: 50, height: 10, child: Icon(Icons.menu)),
-                      child: Icon(Icons.menu),
-                      onTap: () {},
-                    ),
-                  ),
-                ),
-                Flexible(
-                  child: Container(
-                    child: Row(
-                      children: <Widget>[
-                        Text(
-                          'dog',
-                          //model.categories[index].categoryName,
-                          textScaleFactor: 0.75,
-                          textAlign: TextAlign.center,
-                          softWrap: true,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
-      ),
-    ));
   }
 
   Widget _buildAppBar(BuildContext context, HomeViewModel model) {
@@ -107,7 +63,6 @@ class HomeView extends StatelessWidget {
       pinned: false,
       floating: true,
       flexibleSpace: _buildSearchField(context, model),
-      // title:
     );
   }
 
@@ -147,11 +102,17 @@ class HomeView extends StatelessWidget {
 
       ///Lazy building of list
       delegate: SliverChildBuilderDelegate(
-        ((BuildContext context, index) => GestureDetector(
-              onTap: () => model.goToItemDetailPage(model.items[index]),
-              child: ItemTile(
-                item: model.items[index],
-                onPressed: () => model.goToItemDetailPage(model.items[index]),
+        ((BuildContext context, index) => CreationAwareListItem(
+              itemCreated: () {
+                // when the item is created we request more data when it's the 20th index
+                if (index % 20 == 0) model.requestMoreData();
+              },
+              child: GestureDetector(
+                onTap: () => model.goToItemDetailPage(model.items[index]),
+                child: ItemTile(
+                  item: model.items[index],
+                  onPressed: () => model.goToItemDetailPage(model.items[index]),
+                ),
               ),
             )),
 
@@ -160,19 +121,4 @@ class HomeView extends StatelessWidget {
       ),
     );
   }
-
-  //TODO : Double tap back to exit.
-  Future<bool> onWillPop() {
-    DateTime currentBackPressTime;
-    DateTime now = DateTime.now();
-    if (currentBackPressTime == null ||
-        now.difference(currentBackPressTime) > Duration(seconds: 2)) {
-      currentBackPressTime = now;
-      // Fluttertoast.showToast(msg: exit_warning);
-      return Future.value(false);
-    }
-    return Future.value(true);
-  }
-
-//EOF
 }
