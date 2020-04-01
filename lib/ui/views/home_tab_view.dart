7b275e8e90f174b93ce39123840e2ed42a81868d
Marc Christian E. Pragata <mpragata@gmail.com>
2020-04-01 19:51:55 +0800

    added confirmation on exit

--- lib/ui/views/home_tab_view.dart
+++ lib/ui/views/home_tab_view.dart
@@ -1,5 +1,6 @@
 import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:renty_crud_version/ui/views/accounts_view.dart';
 import 'package:renty_crud_version/ui/views/home_view.dart';
 
@@ -8,9 +9,13 @@ class HomeTabView extends StatefulWidget {
   _HomeTabViewState createState() => _HomeTabViewState();
 }
 
-class _HomeTabViewState extends State<HomeTabView> {
+class _HomeTabViewState extends State<HomeTabView>
+    with AutomaticKeepAliveClientMixin {
   int _currentIndex = 0;
   PageController _pageController;
+
+  @override
+  bool get wantKeepAlive => true;
 
   @override
   void initState() {
@@ -33,50 +38,70 @@ class _HomeTabViewState extends State<HomeTabView> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SizedBox.expand(
-          child: PageView(
-            controller: _pageController,
-            onPageChanged: (index) {
-              setState(() => _currentIndex = index);
-            },
-            children: pages,
-          ),
-        ),
-        bottomNavigationBar: BottomNavyBar(
-          selectedIndex: _currentIndex,
-          onItemSelected: (index) {
-            setState(() => _currentIndex = index);
-            _pageController.jumpToPage(index);
-          },
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          items: <BottomNavyBarItem>[
-            BottomNavyBarItem(
-                title: Text('Home'),
-                textAlign: TextAlign.center,
-                icon: Icon(Icons.home),
-                activeColor: Colors.pink,
-                inactiveColor: Colors.grey),
-            BottomNavyBarItem(
-                title: Text('My Items'),
-                textAlign: TextAlign.center,
-                icon: Icon(Icons.storage),
-                activeColor: Colors.pink,
-                inactiveColor: Colors.grey),
-            BottomNavyBarItem(
-                title: Text('Transactions'),
-                textAlign: TextAlign.center,
-                icon: Icon(Icons.history),
-                activeColor: Colors.pink,
-                inactiveColor: Colors.grey),
-            BottomNavyBarItem(
-                title: Text('Account'),
-                icon: Icon(Icons.person),
-                textAlign: TextAlign.center,
-                activeColor: Colors.pink,
-                inactiveColor: Colors.grey),
+    super.build(context);
+    return WillPopScope(
+      onWillPop: () => showDialog<bool>(
+        context: context,
+        builder: (c) => AlertDialog(
+          title: Text('Warning'),
+          content: Text('Do you really want to exit'),
+          actions: [
+            FlatButton(
+              child: Text('Yes'),
+              onPressed: () => SystemNavigator.pop(),
+            ),
+            FlatButton(
+              child: Text('No'),
+              onPressed: () => Navigator.pop(c, false),
+            ),
           ],
+        ),
+      ),
+      child: SafeArea(
+        child: Scaffold(
+          body: SizedBox.expand(
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: (index) {
+                setState(() => _currentIndex = index);
+              },
+              children: pages,
+            ),
+          ),
+          bottomNavigationBar: BottomNavyBar(
+            selectedIndex: _currentIndex,
+            onItemSelected: (index) {
+              setState(() => _currentIndex = index);
+              _pageController.jumpToPage(index);
+            },
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            items: <BottomNavyBarItem>[
+              BottomNavyBarItem(
+                  title: Text('Home'),
+                  textAlign: TextAlign.center,
+                  icon: Icon(Icons.home),
+                  activeColor: Colors.pink,
+                  inactiveColor: Colors.grey),
+              BottomNavyBarItem(
+                  title: Text('My Items'),
+                  textAlign: TextAlign.center,
+                  icon: Icon(Icons.storage),
+                  activeColor: Colors.pink,
+                  inactiveColor: Colors.grey),
+              BottomNavyBarItem(
+                  title: Text('Transactions'),
+                  textAlign: TextAlign.center,
+                  icon: Icon(Icons.history),
+                  activeColor: Colors.pink,
+                  inactiveColor: Colors.grey),
+              BottomNavyBarItem(
+                  title: Text('Account'),
+                  icon: Icon(Icons.person),
+                  textAlign: TextAlign.center,
+                  activeColor: Colors.pink,
+                  inactiveColor: Colors.grey),
+            ],
+          ),
         ),
       ),
     );
