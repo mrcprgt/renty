56e16699d3cd5b9ef7bef5d104a8ba5fee172cf3
Marc Christian E. Pragata <mpragata@gmail.com>
2020-04-01 19:52:55 +0800

    added current user

--- lib/viewmodels/base_model.dart
+++ lib/viewmodels/base_model.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/widgets.dart';
+import 'package:renty_crud_version/models/user.dart';
+import 'package:renty_crud_version/services/authentication_service.dart';
+
+import '../locator.dart';
 
 class BaseModel extends ChangeNotifier {
+  final AuthenticationService _authenticationService =
+      locator<AuthenticationService>();
+
+  User get currentUser => _authenticationService.currentUser;
   bool _busy = false;
   bool get busy => _busy;
 
