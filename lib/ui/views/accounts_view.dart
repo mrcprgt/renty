1957d5028303d98e2d068992c8e165b13a9c9272
Marc Christian E. Pragata <mpragata@gmail.com>
2020-04-02 18:16:10 +0800

    added options to accounts view - no functionalities yet

--- lib/ui/views/accounts_view.dart
+++ lib/ui/views/accounts_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider_architecture/viewmodel_provider.dart';
+import 'package:renty_crud_version/ui/shared/shared_styles.dart';
+import 'package:renty_crud_version/ui/shared/ui_helpers.dart';
 import 'package:renty_crud_version/viewmodels/accounts_view_model.dart';
 
 class AccountsView extends StatefulWidget {
@@ -15,10 +17,13 @@ class _AccountsViewState extends State<AccountsView> {
       builder: (context, model, child) => SafeArea(
         child: SafeArea(
           child: Scaffold(
-              body: CustomScrollView(slivers: <Widget>[
-            // SliverPersistentHeader(delegate: ,)
-            _buildHeader(context, model)
-          ])),
+              body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: CustomScrollView(slivers: <Widget>[
+              _buildHeader(context, model),
+              _myListView(context)
+            ]),
+          )),
         ),
       ),
     );
@@ -28,17 +33,16 @@ class _AccountsViewState extends State<AccountsView> {
     return SliverAppBar(
       automaticallyImplyLeading: false,
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      pinned: false,
-      floating: true,
-      elevation: 2,
-      expandedHeight: 300,
+      // backgroundColor: Colors.pinkAccent,
+      pinned: true,
+      floating: false,
+      expandedHeight: MediaQuery.of(context).size.height / 2 - 40,
       flexibleSpace: Column(
         children: <Widget>[
-          _buildVerificationBanner(model),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: _buildAccountHeaders(model),
-          ),
+          !model.displayBanner()
+              ? _buildVerificationBanner(model)
+              : Container(),
+          _buildAccountHeaders(model),
         ],
       ),
     );
@@ -49,11 +53,18 @@ class _AccountsViewState extends State<AccountsView> {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
         Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            Text(
+              "My Account",
+              style: TextStyle(fontSize: 36),
+            ),
+            verticalSpaceSmall,
             Text(model.getUserDetails()[0]),
             Text(model.getUserDetails()[1]),
-            Text("Account Status: " + model.getUserDetails()[2].toString()),
+            model.getUserDetails()[2]
+                ? Text("Account Status: Verified")
+                : Text("Account Status: Unverified")
           ],
         ),
         Column(
@@ -64,23 +75,75 @@ class _AccountsViewState extends State<AccountsView> {
               image: AssetImage("assets/images/logo.png"),
             )
           ],
-        )
+        ),
       ],
     );
   }
 
-  GestureDetector _buildVerificationBanner(AccountsViewModel model) {
-    return GestureDetector(
-      onTap: () => model.goToVerification(),
-      child: SizedBox(
-        height: 75,
-        child: Container(
-          padding: EdgeInsets.all(8),
-          color: Colors.red,
-          child:
-              Text("YOUR ACCOUNT IS NOT YET VERIFIED! TAP HERE TO VERIFY NOW!"),
-        ),
+  Widget _myListView(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildListDelegate.fixed(
+        <Widget>[
+          Card(
+              child: Material(
+                  color: Colors.white,
+                  child: InkWell(
+                      onTap: () {},
+                      child: ListTile(
+                        leading: Icon(Icons.arrow_upward),
+                        title: Text('Update Account Details'),
+                        trailing: Icon(Icons.keyboard_arrow_right),
+                      )))),
+          Card(
+            child: Material(
+                color: Colors.white,
+                child: InkWell(
+                    onTap: () {},
+                    child: ListTile(
+                      leading: Icon(Icons.storage),
+                      title: Text('My Items'),
+                      trailing: Icon(Icons.keyboard_arrow_right),
+                    ))),
+          ),
+          Card(
+            child: Material(
+                color: Colors.white,
+                child: InkWell(
+                    onTap: () {},
+                    child: ListTile(
+                      leading: Icon(Icons.question_answer),
+                      title: Text('Frequently Asked Questions'),
+                      trailing: Icon(Icons.keyboard_arrow_right),
+                    ))),
+          ),
+          Card(
+            child: Material(
+                color: Colors.white,
+                child: InkWell(
+                    onTap: () {},
+                    child: ListTile(
+                      leading: Icon(Icons.exit_to_app),
+                      title: Text('Logout'),
+                      trailing: Icon(Icons.keyboard_arrow_right),
+                    ))),
+          )
+        ],
       ),
     );
   }
+}
+
+GestureDetector _buildVerificationBanner(AccountsViewModel model) {
+  return GestureDetector(
+    onTap: () => model.goToVerification(),
+    child: SizedBox(
+      height: 75,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        color: Colors.red,
+        child:
+            Text("YOUR ACCOUNT IS NOT YET VERIFIED! TAP HERE TO VERIFY NOW!"),
+      ),
+    ),
+  );
 }
